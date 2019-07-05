dfeaea43ccc1f9b89da4ccea8ab89a43a6ae3fbd
Riwen Mao <rimao@ucsc.edu>
2019-07-05 16:33:10 -0700

    Fixed home page problem

--- slugsbusschedule/src/main/java/me/riwenmao/slugsbusschedule/HomeController.java
+++ slugsbusschedule/src/main/java/me/riwenmao/slugsbusschedule/HomeController.java
@@ -1,14 +1,15 @@
 package me.riwenmao.slugsbusschedule;
 
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("")
-    public String index(){
+    @RequestMapping("/")
+    public String home(){
         System.out.println("Hahahahahaha");
-        return "index.html";
+        return "home.html";
     }
 }
